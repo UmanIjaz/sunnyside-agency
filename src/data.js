2fdce1c2dd7c9444f764c252a2f31bb65017399f
umanijaz <umanijaz5@gmail.com>
2025-03-23 16:14:37 +0500

    Testimonials section complete

---
+++ src/data.js
@@ -0,0 +1,46 @@
+const testimonialData = [
+  {
+    image: "/assets/images/image-emily.jpg",
+    text: "We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.",
+    name: "Emily R.",
+    role: "Marketing Director",
+  },
+  {
+    image: "/assets/images/image-thomas.jpg",
+    text: "Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying experience.",
+    name: "Thomas S.",
+    role: "Chief Operating Officer",
+  },
+  {
+    image: "/assets/images/image-jennie.jpg",
+    text: "Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!",
+    name: "Jennie F.",
+    role: "Business Owner",
+  },
+];
+
+export { testimonialData };
+
+const galleryImages = [
+  {
+    mobileSrc: "/assets/images/mobile/image-gallery-milkbottles.jpg",
+    desktopSrc: "/assets/images/desktop/image-gallery-milkbottles.jpg",
+    alt: "Milk bottles",
+  },
+  {
+    mobileSrc: "/assets/images/mobile/image-gallery-orange.jpg",
+    desktopSrc: "/assets/images/desktop/image-gallery-orange.jpg",
+    alt: "Orange fruit",
+  },
+  {
+    mobileSrc: "/assets/images/mobile/image-gallery-cone.jpg",
+    desktopSrc: "/assets/images/desktop/image-gallery-cone.jpg",
+    alt: "Ice cream cone",
+  },
+  {
+    mobileSrc: "/assets/images/mobile/image-gallery-sugarcubes.jpg",
+    desktopSrc: "/assets/images/desktop/image-gallery-sugarcubes.jpg",
+    alt: "Sugarubes",
+  },
+];
+export { galleryImages };
